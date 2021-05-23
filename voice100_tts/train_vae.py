1d1a194bd43f15be91e1924eba4cf921b13f16fc
Katsuya Iida <katsuya.iida@gmail.com>
2021-05-23 14:21:55 +0000

    update

--- voice100_tts/train_vae.py
+++ voice100_tts/train_vae.py
@@ -2,8 +2,9 @@
 
 from argparse import ArgumentParser
 import torch
+from torch import nn
 import torch.nn.functional as F
-from .yamnet import create_model
+from .models import Voice100Encoder
 from .dataset import get_vc_input_fn
 import pytorch_lightning as pl
 
@@ -11,17 +12,20 @@ class Voice100AutoEncoder(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
-        self.model = create_model()
+        self.model = Voice100Encoder()
+        self.dense = nn.Linear(1024, 27)
 
     def forward(self, melspec):
         return self.model(melspec)
 
     def training_step(self, batch, batch_idx):
         melspec, melspec_len, audio, audio_len = batch
-        audio_hat, _ = self.model(melspec)
+        embeddings = self.model(melspec)
+        print(embeddings.shape, audio.shape, melspec.shape)
+        audio_hat = self.dense(embeddings)
         loss = F.mse_loss(audio_hat, audio, reduction='none')
         loss_weights = (torch.arange(audio.shape[1]).to(audio.device)[None, :] < audio_len[:, None]).float()
-        loss = torch.sum(loss * loss_weights[:, :, None]) / torch.sum(loss_weights)
+        loss = torch.sum(loss * loss_weights[:, :, None]) / torch.sum(loss_weights) / 27
         self.log('train_loss', loss)
         return loss
 
@@ -37,9 +41,10 @@ class Voice100AutoEncoder(pl.LightningModule):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--dataset', default='kokoro_tiny', help='Analyze F0 of sampled data.')
+    parser.add_argument('--sample_rate', default=16000, type=int, help='Sampling rate')
     parser = pl.Trainer.add_argparse_args(parser)
     parser = Voice100AutoEncoder.add_model_specific_args(parser)
-    parser.add_argument('--dataset', default='kokoro_tiny', help='Analyze F0 of sampled data.')
     args = parser.parse_args()
     if False:
         parser.add_argument('--train', action='store_true', help='Split audio and encode with WORLD vocoder.')
@@ -63,5 +68,5 @@ if __name__ == '__main__':
     #if False:
     #else:
     #    autoencoder = Voice100AutoEncoder.load_from_checkpoint('./lightning_logs/version_6/checkpoints/epoch=67-step=93635.ckpt')
-    train_loader = get_vc_input_fn(args, 16000, 64, 27)
+    train_loader = get_vc_input_fn(args, 64, 27)
     trainer.fit(autoencoder, train_loader)
