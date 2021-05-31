83705672ef03813e9c98f2019826f28be3b491ca
Katsuya Iida <katsuya.iida@gmail.com>
2021-05-31 21:34:56 +0900

    Refactor vocoders and implement Mel/MFCC decode (#3)

--- voice100/vocoder.py
+++ voice100/vocoder.py
@@ -1,26 +1,38 @@
-import torch
-import torchaudio
+# Copyright (C) 2021 Katsuya Iida. All rights reserved.
+
+from abc import ABC, abstractmethod
 import numpy as np
 import pyworld
 import pysptk
 import librosa
 import math
 
-from .wsola import WSOLA
+class Vocoder(ABC):
 
-SAMPLE_RATE = 16000
+    @abstractmethod
+    def encode(self, waveform: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
 
-class WORLDVocoder:
+    @abstractmethod
+    def decode(self, encoded: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+
+class WORLDVocoder(Vocoder):
+
     def __init__(self, sample_rate=16000, frame_period=10.0, n_fft=512, mcep_dim=24, mcep_alpha=0.410):
+        super().__init__()
         self.sample_rate = sample_rate
         self.frame_period = frame_period
         self.n_fft = n_fft
         self.mcep_dim = mcep_dim
         self.mcep_alpha = mcep_alpha
+        self.codeap_dim = 1
+        self.encoded_dim = 1 + (self.mcep_dim + 1) + self.codeap_dim
 
-    def encode(self, waveform, f0_floor=40, f0_ceil=400):
+    def encode(self, waveform, f0_floor=80, f0_ceil=400):
         waveform = waveform.astype(np.double)
-        f0, time_axis = pyworld.harvest(waveform, self.sample_rate, f0_floor=f0_floor, f0_ceil=f0_ceil, frame_period=self.frame_period)
+        f0, time_axis = pyworld.dio(waveform, self.sample_rate, f0_floor=f0_floor,
+            f0_ceil=f0_ceil, frame_period=self.frame_period)
         spc = pyworld.cheaptrick(waveform, f0, time_axis, self.sample_rate, fft_size=self.n_fft)
         ap = pyworld.d4c(waveform, f0, time_axis, self.sample_rate, fft_size=self.n_fft)
 
@@ -41,33 +53,99 @@ class WORLDVocoder:
         waveform = pyworld.synthesize(f0, spc, ap, self.sample_rate, frame_period=self.frame_period)
         return waveform
 
-class MelSpectrogramVocoder:
+class MelSpectrogramVocoder(Vocoder):
+
     def __init__(self, sample_rate=16000, n_fft=512, win_length=400, hop_length=160, n_mels=64, log_offset=1e-6):
+        super().__init__()
         self.sample_rate = sample_rate
         self.n_fft = n_fft
         self.win_length = win_length
         self.hop_length = hop_length
         self.n_mels = n_mels
         self.log_offset = log_offset
+        self.encoded_dim = n_mels
 
     def encode(self, waveform):
         melspec = librosa.feature.melspectrogram(
             waveform,
             sr=self.sample_rate,
             n_fft=self.n_fft,
-            hop_length=self.hop_length,
             win_length=self.win_length,
+            hop_length=self.hop_length,
             center=True,
             pad_mode="reflect",
             power=2.0,
             n_mels=self.n_mels,
             norm='slaney',
-            htk=True,
-        )
+            htk=True)
         return np.log(melspec.T + self.log_offset).astype(np.float32)
 
-    def decode(self, x):
-        raise NotImplementedError("Decoding from Mel-spectrogram is not supported")
+    def decode(self, encoded):
+        decoded = librosa.feature.inverse.mel_to_audio(
+            np.exp(encoded.T) - self.log_offset,
+            sr=self.sample_rate,
+            n_fft=self.n_fft,
+            win_length=self.win_length,
+            hop_length=self.hop_length,
+            center=True,
+            pad_mode="reflect",
+            power=2.0,
+            norm='slaney',
+            htk=True)
+        return decoded
+
+class MFCCVocoder(Vocoder):
+
+    def __init__(self, sample_rate=16000, n_fft=512, win_length=400, hop_length=160, n_mels=64, n_mfcc=20):
+        super().__init__()
+        self.sample_rate = sample_rate
+        self.n_fft = n_fft
+        self.win_length = win_length
+        self.hop_length = hop_length
+        self.n_mels = n_mels
+        self.n_mfcc = n_mfcc
+        self.encoded_dim = n_mfcc
+
+    def encode(self, waveform):
+        melspec = librosa.feature.melspectrogram(
+            y=waveform,
+            sr=self.sample_rate,
+            n_fft=self.n_fft,
+            hop_length=self.hop_length,
+            win_length=self.win_length,
+            n_mels=self.n_mels,
+            center=True,
+            pad_mode="reflect",
+            htk=True,
+            norm=None)
+        
+        mfcc = librosa.feature.mfcc(
+            S=librosa.core.spectrum.power_to_db(melspec),
+            n_mfcc=self.n_mfcc,
+            dct_type=2,
+            norm='ortho')
+
+        return mfcc.T.astype(np.float32)
+
+    def decode(self, encoded):
+        melspec = librosa.feature.inverse.mfcc_to_mel(
+            encoded.T,
+            n_mels=self.n_mels,
+            dct_type=2,
+            norm='ortho')
+
+        decoded = librosa.feature.inverse.mel_to_audio(
+            melspec,
+            sr=self.sample_rate,
+            n_fft=self.n_fft,
+            hop_length=self.hop_length,
+            win_length=self.win_length,
+            center=True,
+            pad_mode="reflect",
+            norm=None,
+            htk=True)
+
+        return decoded
 
 class AudioAugmentation:
     def __init__(self, sample_rate=16000):
@@ -106,3 +184,11 @@ class AudioAugmentation:
         waveform = (waveform * tone_rate + tone) / (1 + tone_rate)
 
         return waveform.astype(np.float32)
+
+__all__ = [
+    "Vocoder",
+    "WORLDVocoder",
+    "MelSpectrogramVocoder",
+    "MFCCVocoder",
+    "AudioAugmentation",
+    ]
