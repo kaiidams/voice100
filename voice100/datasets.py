132754ad2f64afe178d8ca71234fe237e8ddc758
Katsuya Iida <katsuya.iida@gmail.com>
2021-06-02 11:33:57 +0900

    update

--- voice100/datasets.py
+++ voice100/datasets.py
@@ -37,7 +37,7 @@ class VoiceDataset(Dataset):
         return audiopath, text
 
 class EncodedVoiceDataset(Dataset):
-    def __init__(self, dataset, repeat=1, cachedir=None):
+    def __init__(self, dataset, repeat=5, cachedir=None):
         self._dataset = dataset
         self._cachedir = cachedir
         self._repeat = repeat
