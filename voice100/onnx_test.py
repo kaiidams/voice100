5948424c99deeb7b562afb7eee83aea08887d642
Katsuya Iida <katsuya.iida@gmail.com>
2021-06-08 18:18:38 +0900

    QuartzNet ASR (#5)

---
+++ voice100/onnx_test.py
@@ -0,0 +1,27 @@
+import time
+import onnxruntime as ort
+import numpy as np
+from voice100.vocoder import MelSpectrogramVocoder
+from voice100.encoder import CharEncoder
+
+sess = ort.InferenceSession('test.onnx')
+import librosa
+waveform, sr = librosa.load('test.flac', 16000)
+vocoder = MelSpectrogramVocoder()
+encoder = CharEncoder()
+audio = vocoder.encode(waveform)
+
+ort_inputs = {
+    'audio': audio[None, :, :], #np.zeros(shape=[1, 123, 64]).astype(dtype=np.float32),
+    'audio_len': np.array([audio.shape[0]], dtype=np.int32)
+}
+
+s = time.time()
+for i in range(100):
+    logits, logits_len = sess.run(['logits', 'logits_len'], ort_inputs)
+    t = time.time()
+    print(t - s)
+    s = t
+
+pred = logits.argmax(-1)
+print(encoder.merge_repeated(encoder.decode(pred[0])))
