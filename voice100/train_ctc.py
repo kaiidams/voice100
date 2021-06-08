1f33e21635ff66447e23bdfe6f0e96e83fe5e24d
Katsuya Iida <katsuya.iida@gmail.com>
2021-06-08 09:10:36 +0900

    update

--- voice100/train_ctc.py
+++ voice100/train_ctc.py
@@ -16,25 +16,38 @@ NUM_LAYERS = 2
 VOCAB_SIZE = CharEncoder().vocab_size
 assert VOCAB_SIZE == 28, VOCAB_SIZE
 
+class LSTMAudioEncoder(nn.Module):
+
+    def __init__(self, audio_size, embed_size, num_layers):
+        super().__init__()
+        self.dense = nn.Linear(audio_size, embed_size, bias=True)
+        self.lstm = nn.LSTM(embed_size, embed_size // 2, num_layers=num_layers, dropout=0.2, bidirectional=True)
+
+    def forward(self, audio, audio_len, enforce_sorted=False):
+        dense_out = self.dense(audio)
+        packed_dense_out = nn.utils.rnn.pack_padded_sequence(dense_out, audio_len, batch_first=True, enforce_sorted=enforce_sorted)
+        packed_lstm_out, _ = self.lstm(packed_dense_out)
+        lstm_out, lstm_out_len = nn.utils.rnn.pad_packed_sequence(packed_lstm_out, batch_first=True)
+        return lstm_out, lstm_out_len
+
 class LSTMAudioToLetter(pl.LightningModule):
 
-    def __init__(self, audio_dim, hidden_dim, vocab_size, num_layers, learning_rate):
+    def __init__(self, audio_size, embed_size, vocab_size, num_layers, learning_rate):
         super().__init__()
         self.save_hyperparameters()
-        self.encoder = nn.LSTM(audio_dim, hidden_dim, num_layers=num_layers, dropout=0.2, bidirectional=True)
-        self.decoder = nn.Linear(hidden_dim * 2, vocab_size, bias=True)
+        self.encoder = LSTMAudioEncoder(audio_size, embed_size, num_layers)
+        self.decoder = nn.Linear(embed_size, vocab_size, bias=True)
         self.loss_fn = nn.CTCLoss()
 
-    def forward(self, audio):
-        lstm_out, _ = self.encoder(audio)
-        lstm_out, lstm_out_len = torch.nn.utils.rnn.pad_packed_sequence(lstm_out, batch_first=True)
+    def forward(self, audio, audio_len):
+        lstm_out, lstm_out_len = self.encoder(audio, audio_len, enforce_sorted=False)
         return self.decoder(lstm_out), lstm_out_len
 
     def _calc_batch_loss(self, batch):
-        audio, text, text_len = batch
-        # audio: packed
+        audio, audio_len, text, text_len = batch
+        # audio: [batch_size, audio_len, audio_size]
         # text: [batch_size, text_len]
-        logits, logits_len = self(audio)
+        logits, logits_len = self(audio, audio_len)
         # logits: [batch_size, audio_len, vocab_size]
         logits = torch.transpose(logits, 0, 1)
         # logits: [audio_len, batch_size, vocab_size]
@@ -79,10 +92,10 @@ def cli_main():
     args.valid_rate = 0.1
     args.repeat = 2
 
-    train_loader, val_loader = get_ctc_input_fn(args, pack_audio=True)
+    train_loader, val_loader = get_ctc_input_fn(args, pack_audio=False)
     model = LSTMAudioToLetter(
-        audio_dim=MELSPEC_DIM,
-        hidden_dim=HIDDEN_DIM,
+        audio_size=MELSPEC_DIM,
+        embed_size=HIDDEN_DIM,
         num_layers=NUM_LAYERS,
         vocab_size=VOCAB_SIZE,
         learning_rate=args.learning_rate)
