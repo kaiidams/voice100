0ec750bc19a188158e20918c9216c81488b06fe3
Katsuya Iida <katsuya.iida@gmail.com>
2021-06-26 11:25:23 +0900

    update

--- voice100/transformer.py
+++ voice100/transformer.py
@@ -63,15 +63,6 @@ def list_variables():
 
 # Misc
 
-def layer_norm(inputs, epsilon=0.001):
-    with variable_scope('layer_normalization'):
-        mean = torch.mean(inputs, axis=-1)[:, :, None] 
-        var = torch.mean((inputs - mean) ** 2, axis=-1)[:, :, None]
-        norm_inputs = (inputs - mean) / torch.sqrt(var + epsilon)
-        gamma = get_variable('gamma')
-        beta = get_variable('beta')
-        return gamma * norm_inputs + beta
-
 def load_numpy_state_layer_norm(layer):
     with variable_scope('layer_normalization'):
         layer.weight.copy_(get_variable('gamma'))
@@ -108,33 +99,6 @@ def load_numpy_state_dense_layer(name, layer):
         layer.weight.copy_(get_variable('kernel'))
         if layer.bias is not None:
             layer.bias.copy_(get_variable('bias'))
-
-def dense_layer(name, inputs, subscripts='abc,cde->abde', use_bias=True, activation=None):
-    with variable_scope(name):
-        kernel = get_variable('kernel')
-        y = torch.einsum(subscripts, inputs, kernel)
-        if use_bias:
-            bias = get_variable('bias')
-            y += bias
-        if False:
-            if not has_current_module():
-                if use_bias:
-                    kernel = get_variable('kernel')
-                    bias = get_variable('bias')
-                    linear = EinSumLinear(subscripts=subscripts, weight_shape=kernel.shape, bias_shape=bias.shape)
-                    linear.weight = kernel
-                    linear.weight = bias
-                else:
-                    kernel = get_variable('kernel')
-                    linear = EinSumLinear(subscripts=subscripts, weight_shape=kernel.shape)
-                    linear.weight = kernel
-                set_current_module(linear)
-            print(current_module())
-            linear = current_module()
-            y = linear(inputs)
-        if activation is not None:
-            y = activation(y)
-        return y
 
 # Transformer layers
 
@@ -181,48 +145,13 @@ def get_decoder_self_attention_bias(length, dtype=torch.float32):
     y = (torch.reshape(r, [-1, 1]) < torch.reshape(r, [1, -1])).to(dtype) * neg_inf
     return y[None, None, :, :]
 
-def get_decoder_self_attention_bias_v2(length, dtype=torch.float32):
-    """Calculate bias for decoder that maintains model's autoregressive property.
-
-    Creates a tensor that masks out locations that correspond to illegal
-    connections, so prediction at position i cannot draw information from future
-    positions.
-
-    Args:
-        length: int length of sequences in batch.
-        dtype: The dtype of the return value.
-
-    Returns:
-        float tensor of shape [1, 1, length, length]
-    """
-    neg_inf = _NEG_INF_FP16 if dtype == torch.float16 else _NEG_INF_FP32
-    with torch.name_scope("decoder_self_attention_bias"):
-        valid_locs = torch.linalg.band_part(torch.ones([length, length], dtype=dtype),
-                                                                         -1, 0)
-        valid_locs = torch.reshape(valid_locs, [1, 1, length, length])
-        decoder_bias = neg_inf * (1.0 - valid_locs)
-    return decoder_bias
-
 def get_position_encoding(
         length, hidden_size, min_timescale=1.0, max_timescale=1.0e4):
     """Return positional encoding.
 
-    Calculates the position encoding as a mix of sine and cosine functions with
-    geometrically increasing wavelengths.
-    Defined and formulized in Attention is All You Need, section 3.5.
-
-    Args:
-        length: Sequence length.
-        hidden_size: Size of the
-        min_timescale: Minimum scale that will be applied at each position
-        max_timescale: Maximum scale that will be applied at each position
-
     Returns:
         Tensor with shape [length, hidden_size]
     """
-    # We compute the positional encoding in float32 even if the model uses
-    # float16, as many of the ops used, like log and exp, are numerically unstable
-    # in float16.
     position = torch.arange(0, length).to(torch.float32)
     num_timescales = hidden_size // 2
     log_timescale_increment = (
@@ -248,15 +177,8 @@ class PrePostProcessingWrapper(nn.Module):
                 self.layer.load_numpy_state()
 
     def forward(self, x, *args):
-        with variable_scope("pre_post_processing_wrapper"):
-            y = layer_norm(x, epsilon=1e-6)
-            y = self.layer(y, *args)
-            return x + y
-
-def pre_post_processing_wrapper(layer, x, *args):
-    with variable_scope("pre_post_processing_wrapper"):
-        y = layer_norm(x, epsilon=1e-6)
-        y = layer(y, *args)
+        y = self.layer_norm(x)
+        y = self.layer(y, *args)
         return x + y
 
 class AttentionLayer(nn.Module):
@@ -278,7 +200,7 @@ class AttentionLayer(nn.Module):
             load_numpy_state_dense_layer('output_transform', self.output_transform)
 
     def forward(self, query_input, source_input, bias):
-        self.attention_layer(query_input, source_input, bias, 'attention')
+        return self.attention_layer(query_input, source_input, bias, 'attention')
 
     def attention_layer(self, query_input, source_input, bias, name):
         with variable_scope(name):
@@ -311,29 +233,6 @@ class SelfAttentionLayer(AttentionLayer):
     def forward(self, query_input, bias, **args):
         return self.attention_layer(query_input, query_input, bias, name='self_attention', **args)
 
-def self_attention_layer(query_input, bias, name="self_attention", **args):
-    return attention_layer(query_input, query_input, bias, name=name, **args)
-
-def attention_layer(query_input, source_input, bias, name="attention", hidden_size=512, num_heads=8):
-    with variable_scope(name):
-        query = dense_layer('query', query_input, use_bias=False)
-        key = dense_layer('key', source_input, use_bias=False)
-        value = dense_layer('value', source_input, use_bias=False)
-
-        depth = (hidden_size // num_heads)
-        query *= depth ** -0.5
-
-        logits = torch.einsum("btnh,bfnh->bnft", key, query)
-        if bias is not None:
-            logits += bias
-        weights = torch.softmax(logits, dim=3)
-        attention_output = torch.einsum("bnft,btnh->bfnh", weights, value)
-
-        attention_output = dense_layer('output_transform', attention_output,
-                                         subscripts='abcd,cde->abe',
-                                         use_bias=False)
-    return attention_output
-
 class FeedForwardNetwork(nn.Module):
 
     def __init__(self, hidden_size, filter_size):
@@ -360,12 +259,6 @@ class FeedForwardNetwork(nn.Module):
             x = self.output_layer(x)
             return x
 
-def feed_forward_network(x):
-    with variable_scope("feed_forward_network"):
-        output = dense_layer('filter_layer', x, subscripts='abc,cd->abd', activation=torch.relu)
-        output = dense_layer('output_layer', output, subscripts='abc,cd->abd')
-    return output
-
 # Transformer
 
 class TransformerEncoderLayer(nn.Module):
@@ -383,10 +276,8 @@ class TransformerEncoderLayer(nn.Module):
             self.ffn.load_numpy_state()
 
     def forward(self, inputs, attention_bias):
-        with variable_scope("self_attention"):
-            x = self.self_attention(inputs, attention_bias)
-        with variable_scope("ffn"):
-            x = self.ffn(x)
+        x = self.self_attention(inputs, attention_bias)
+        x = self.ffn(x)
         return x
 
 class TransformerEncoder(nn.Module):
@@ -398,8 +289,8 @@ class TransformerEncoder(nn.Module):
         layers = []
         for i in range(self.num_layers):
             layers.append(TransformerEncoderLayer(hidden_size=hidden_size, filter_size=filter_size, num_heads=num_heads))
-        layers.append(nn.LayerNorm(hidden_size, eps=1e-6))
-        self.layers = nn.Sequential(*layers)
+        self.layers = nn.ModuleList(layers)
+        self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-6)
 
     def load_numpy_state(self):
         with variable_scope("encode"):
@@ -408,73 +299,85 @@ class TransformerEncoder(nn.Module):
                     with variable_scope("layer_%d" % n):
                         self.layers[n].load_numpy_state()
 
-                load_numpy_state_layer_norm(self.layers[self.num_layers])
+                load_numpy_state_layer_norm(self.layer_norm)
 
     def forward(self, inputs, embedded_inputs):
-        with variable_scope('encode'):
-            attention_bias = get_padding_bias(inputs, dtype=embedded_inputs.dtype)
-            length = embedded_inputs.shape[1]
-            pos_encoding = get_position_encoding(length, self.hidden_size)
-            pos_encoding = pos_encoding.to(embedded_inputs.dtype)
-            encoder_inputs = embedded_inputs + pos_encoding
-
-            return self.encoder_stack(encoder_inputs, attention_bias), attention_bias
+        attention_bias = get_padding_bias(inputs, dtype=embedded_inputs.dtype)
+        length = embedded_inputs.shape[1]
+        pos_encoding = get_position_encoding(length, self.hidden_size)
+        pos_encoding = pos_encoding.to(embedded_inputs.dtype)
+        encoder_inputs = embedded_inputs + pos_encoding
+        return self.encoder_stack(encoder_inputs, attention_bias), attention_bias
 
     def encoder_stack(self, encoder_inputs, attention_bias):
-        with variable_scope('encoder_stack'):
-            for n in range(self.num_layers):
-                with variable_scope("layer_%d" % n):
-                    encoder_inputs = self.layers[n](encoder_inputs, attention_bias)
+        for layer in self.layers:
+            encoder_inputs = layer(encoder_inputs, attention_bias)
+        return self.layer_norm(encoder_inputs)
 
-        return self.layers[self.num_layers](encoder_inputs)
+class TransformerDecoderLayer(nn.Module):
+
+    def __init__(self, hidden_size: int, filter_size: int, num_heads: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.self_attention = PrePostProcessingWrapper(SelfAttentionLayer(hidden_size, num_heads), hidden_size)
+        self.encdec_attention = PrePostProcessingWrapper(AttentionLayer(hidden_size, num_heads), hidden_size)
+        self.ffn = PrePostProcessingWrapper(FeedForwardNetwork(hidden_size, filter_size), hidden_size)
+
+    def load_numpy_state(self):
+        with variable_scope("self_attention"):
+            self.self_attention.load_numpy_state()
+        with variable_scope("encdec_attention"):
+            self.encdec_attention.load_numpy_state()
+        with variable_scope("ffn"):
+            self.ffn.load_numpy_state()
+
+    def forward(self, decoder_inputs, encoder_outputs,
+        decoder_self_attention_bias, attention_bias):
+        x = self.self_attention(decoder_inputs, decoder_self_attention_bias)
+        x = self.encdec_attention(x, encoder_outputs, attention_bias)
+        x = self.ffn(x)
+        return x
 
 class TransformerDecoder(nn.Module):
 
-    def __init__(self, num_layers: int, hidden_size: int):
+    def __init__(self, num_layers: int, hidden_size: int, filter_size: int, num_heads: int):
         super().__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
+        self.layers = nn.ModuleList()
+        for i in range(self.num_layers):
+            self.layers.append(TransformerDecoderLayer(hidden_size=hidden_size, filter_size=filter_size, num_heads=num_heads))
+        self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+
+    def load_numpy_state(self):
+        with variable_scope("decode"):
+            with variable_scope('decoder_stack'):
+                for n in range(self.num_layers):
+                    with variable_scope("layer_%d" % n):
+                        self.layers[n].load_numpy_state()
+
+                load_numpy_state_layer_norm(self.layer_norm)
 
     def forward(self, embedded_targets, encoder_outputs, attention_bias):
-        with variable_scope("decode"):
-            length = embedded_targets.shape[1]
-            pos_encoding = get_position_encoding(length, self.hidden_size)
-            pos_encoding = pos_encoding.to(embedded_targets.dtype)
-            decoder_inputs = embedded_targets + pos_encoding
+        length = embedded_targets.shape[1]
+        pos_encoding = get_position_encoding(length, self.hidden_size)
+        pos_encoding = pos_encoding.to(embedded_targets.dtype)
+        decoder_inputs = embedded_targets + pos_encoding
 
-            decoder_self_attention_bias = get_decoder_self_attention_bias(length, dtype=embedded_targets.dtype)
-            outputs = self.decoder_stack(
-                decoder_inputs,
-                encoder_outputs,
-                decoder_self_attention_bias,
-                attention_bias)
-
-        return outputs
+        decoder_self_attention_bias = get_decoder_self_attention_bias(length, dtype=embedded_targets.dtype)
+        return self.decoder_stack(
+            decoder_inputs,
+            encoder_outputs,
+            decoder_self_attention_bias,
+            attention_bias)
 
     def decoder_stack(
-        self, decoder_inputs, encoder_outputs,
-        decoder_self_attention_bias, attention_bias
+        self, decoder_inputs, encoder_outputs, decoder_self_attention_bias, attention_bias
         ):
-        with variable_scope('decoder_stack'):
-            for n in range(self.num_layers):
-                with variable_scope("layer_%d" % n):
-                    with variable_scope("self_attention"):
-                        decoder_inputs = pre_post_processing_wrapper(
-                            self_attention_layer,
-                            decoder_inputs,
-                            decoder_self_attention_bias)
-                    with variable_scope("encdec_attention"):
-                        decoder_inputs = pre_post_processing_wrapper(
-                            attention_layer,
-                            decoder_inputs,
-                            encoder_outputs,
-                            attention_bias)
-                    with variable_scope("ffn"):
-                        decoder_inputs = pre_post_processing_wrapper(
-                            feed_forward_network,
-                            decoder_inputs)
-
-            return layer_norm(decoder_inputs, epsilon=1e-6)
+        x = decoder_inputs
+        for layer in self.layers:
+            x = layer(x, encoder_outputs, decoder_self_attention_bias, attention_bias)
+        return self.layer_norm(x)
 
 class Transformer(nn.Module):
     __constants__ = ['vocab_size', 'hidden_size']
@@ -485,7 +388,7 @@ class Transformer(nn.Module):
         self.hidden_size = hidden_size
         self.dtype = torch.float32
         self.encode = TransformerEncoder(hidden_size=hidden_size, filter_size=filter_size, num_layers=num_layers, num_heads=num_heads)
-        self.decode = TransformerDecoder(hidden_size=hidden_size, num_layers=num_layers)
+        self.decode = TransformerDecoder(hidden_size=hidden_size, filter_size=filter_size, num_layers=num_layers, num_heads=num_heads)
         self.embedding_softmax_layer = EmbeddingSharedWeights(self.vocab_size, self.hidden_size)
         if arr is not None:
             set_variables(arr)
@@ -508,6 +411,7 @@ class Transformer(nn.Module):
 
     def load_numpy_state(self):
         self.encode.load_numpy_state()
+        self.decode.load_numpy_state()
 
 class InferTransformer(Transformer):
     __constants__ = ['vocab_size', 'hidden_size']
