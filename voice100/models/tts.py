cc691dfdd6056a927a9840d8fe8933d914c20dc5
Katsuya Iida <katsuya.iida@gmail.com>
2021-08-09 10:16:24 +0900

    update

--- voice100/models/tts.py
+++ voice100/models/tts.py
@@ -131,6 +131,8 @@ class TextToAlignTextModel(pl.LightningModule):
         self.layers = nn.Sequential(
             InvertedResidual(hidden_size, hidden_size, kernel_size=35),
             InvertedResidual(hidden_size, hidden_size, kernel_size=35),
+            InvertedResidual(hidden_size, hidden_size, kernel_size=35),
+            InvertedResidual(hidden_size, hidden_size, kernel_size=35),
             nn.Conv1d(hidden_size, 2, kernel_size=1, bias=True))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -187,7 +189,7 @@ class TextToAlignTextModel(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--hidden_size', type=int, default=256)
+        parser.add_argument('--hidden_size', type=int, default=128)
         parser.add_argument('--learning_rate', type=float, default=1e-3)
         return parser
 
