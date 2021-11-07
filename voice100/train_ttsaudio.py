ea5f3be2829893e116f639781756412c3c6e50e1
Katsuya Iida <katsuya.iida@gmail.com>
2021-11-07 18:43:44 +0900

    update

--- voice100/train_ttsaudio.py
+++ voice100/train_ttsaudio.py
@@ -23,7 +23,7 @@ def cli_main():
 
     data = AudioTextDataModule.from_argparse_args(args)
     model = AlignTextToAudioModel.from_argparse_args(args)
-    checkpoint_callback = ModelCheckpoint(monitor='val_loss', save_last=True, period=10)
+    checkpoint_callback = ModelCheckpoint(monitor='val_loss', save_last=True, every_n_epochs=10)
     trainer = pl.Trainer.from_argparse_args(
         args,
         callbacks=[checkpoint_callback])
