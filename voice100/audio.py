849da8cb43dfc5057e08416ec94b9621f1368658
Katsuya Iida <katsuya.iida@gmail.com>
2022-01-08 20:35:35 +0900

    update

--- voice100/audio.py
+++ voice100/audio.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2021 Katsuya Iida. All rights reserved.
 
-from typing import Tuple
 import torch
 from torch import nn
 import random
@@ -19,9 +18,7 @@ class BatchSpectrogramAugumentation(nn.Module):
         self.do_timestretch = do_timestretch
 
     @torch.no_grad()
-    def forward(
-        self, audio: torch.Tensor, audio_len: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, audio, audio_len):
         assert len(audio.shape) == 3
 
         if self.do_timestretch and random.random() < AUGUMENT_RATE:
@@ -36,8 +33,7 @@ class BatchSpectrogramAugumentation(nn.Module):
             audio = self.mixnoise(audio)
         if random.random() < AUGUMENT_RATE:
             audio = self.mixaudio(audio, audio_len)
-        else:
-            audio = self.maskaudio(audio, audio_len)
+
         return audio, audio_len
 
     def timestretch(self, audio, audio_len):
@@ -87,7 +83,3 @@ class BatchSpectrogramAugumentation(nn.Module):
         x = torch.exp(audio) * audio_mask
         y = torch.cat([x[1:], x[:1]], axis=0)
         return torch.log(0.9 * x + 0.1 * y + 1e-15) * audio_mask
-
-    def maskaudio(self, audio, audio_len):
-        audio_mask = (torch.arange(audio.shape[1], device=audio.device)[None, :, None] < audio_len[:, None, None]).float()
-        return audio * audio_mask
