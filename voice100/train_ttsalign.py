208fa351576d03451d0eed4d4d523a0226340cb6
Katsuya Iida <katsuya.iida@gmail.com>
2022-01-08 21:03:40 +0900

    update

--- voice100/train_ttsalign.py
+++ voice100/train_ttsalign.py
@@ -15,8 +15,9 @@ def cli_main():
     parser = pl.Trainer.add_argparse_args(parser)
     parser = AlignTextDataModule.add_argparse_args(parser)
     parser = TextToAlignTextModel.add_model_specific_args(parser)
+    parser.set_defaults(log_every_n_steps=10)
     args = parser.parse_args()
-    data = AlignTextDataModule.from_argparse_args(args)
+    data: AlignTextDataModule = AlignTextDataModule.from_argparse_args(args)
     model = TextToAlignTextModel.from_argparse_args(args, vocab_size=data.vocab_size)
     checkpoint_callback = ModelCheckpoint(monitor='val_loss', save_last=True, every_n_epochs=10)
     trainer = pl.Trainer.from_argparse_args(
