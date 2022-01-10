6ac659024abb248e3c743aceef625b9aae10e402
Katsuya Iida <katsuya.iida@gmail.com>
2022-01-10 20:44:48 +0900

    Bump version up (#45)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="voice100",
-    version="1.0",
+    version="1.0.1",
     author="Katsuya Iida",
     author_email="katsuya.iida@gmail.com",
     description="Voice100",
