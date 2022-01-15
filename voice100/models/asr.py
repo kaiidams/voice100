e63564113c1776ce1387934b07f1228cc569dcae
Katsuya Iida <katsuya.iida@gmail.com>
2022-01-15 22:18:52 +0900

    update

--- voice100/models/asr.py
+++ voice100/models/asr.py
@@ -148,7 +148,7 @@ class AudioToCharCTC(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--learning_rate', type=float, default=0.00001)
+        parser.add_argument('--learning_rate', type=float, default=0.001)
         parser.add_argument('--weight_decay', type=float, default=0.00004)
         parser.add_argument('--hidden_size', type=float, default=512)
         parser.add_argument('--embed_size', type=float, default=512)
