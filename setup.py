faaa7a8e9c0b71359ea69da046e74b9a4f142e52
Katsuya Iida <katsuya.iida@gmail.com>
2022-04-22 08:28:44 +0900

    New Japanese TTS audio model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="voice100",
-    version="1.3.0",
+    version="1.3.1",
     author="Katsuya Iida",
     author_email="katsuya.iida@gmail.com",
     description="Voice100",
