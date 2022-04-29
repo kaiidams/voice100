f2c55db5adc7e4fbb836864fa354570c46f8303f
Katsuya Iida <katsuya.iida@gmail.com>
2022-04-29 14:24:29 +0900

    update

--- voice100/train_vae.py
+++ voice100/train_vae.py
@@ -13,7 +13,7 @@ from voice100.audio import BatchSpectrogramAugumentation
 
 from voice100.models.asr import ConvVoiceEncoder
 
-from .models.tts import VoiceDecoder, WORLDLoss, WORLDNorm
+from .models.tts import VoiceDecoder, WORLDLoss, WORLDNorm, adjust_size, generate_padding_mask
 from .datasets import (
     get_dataset, get_audio_transform,
     get_text_transform,
@@ -209,6 +209,22 @@ class AudioTextAudioDataModule(pl.LightningDataModule):
         return ["task", "test"]
 
 
+class EncoderLoss(nn.Module):
+    def __init__(self, device=None, dtype=None):
+        super().__init__()
+        self.ce_loss = nn.CrossEntropyLoss(reduction='none')
+
+    def forward(
+        self, logits: torch.Tensor, aligntext: torch.Tensor, aligntext_len: torch.Tensor
+    ) -> torch.Tensor:
+        logits, aligntext = adjust_size(logits, aligntext)
+        mask = generate_padding_mask(aligntext, aligntext_len)
+        logits = torch.transpose(logits, 1, 2)
+        loss = self.ce_loss(logits, aligntext) * mask
+        loss = torch.sum(loss) / torch.sum(mask)
+        return loss
+
+
 class AudioToAudioModel(pl.LightningModule):
     def __init__(
         self, audio_size, vocab_size: int, hidden_size: int, learning_rate: float
@@ -223,31 +239,42 @@ class AudioToAudioModel(pl.LightningModule):
         self.f0_size = 1
         self.logspc_size = self.n_fft // 2 + 1
         self.codeap_size = 1
-        self.encoder = ConvVoiceEncoder(audio_size, hidden_size, hidden_size)
-        self.encoder_proj = nn.Linear(hidden_size, vocab_size)
+        self.voice_encoder = ConvVoiceEncoder(audio_size, hidden_size, hidden_size)
         self.batch_augment = BatchSpectrogramAugumentation()
         self.do_normalize = False
-        #self.embedding = nn.Embedding(vocab_size, hidden_size)
-        self.embedding = nn.Linear(vocab_size, hidden_size)
-        self.audio_size = self.hasf0_size + self.f0_size + self.logspc_size + self.codeap_size
-        self.decoder = VoiceDecoder(hidden_size, self.audio_size)
+        self.embedding = nn.Embedding(vocab_size, hidden_size)
+        self.audio_size = audio_size
+        self.target_audio_size = self.hasf0_size + self.f0_size + self.logspc_size + self.codeap_size
+        self.voice_decoder = VoiceDecoder(hidden_size, self.target_audio_size)
         self.norm = WORLDNorm(self.logspc_size, self.codeap_size)
-        self.criteria = WORLDLoss(sample_rate=self.sample_rate, n_fft=self.n_fft)
+        self.encoder_loss = EncoderLoss()
+        self.decoder_loss = WORLDLoss(sample_rate=self.sample_rate, n_fft=self.n_fft)
 
     def forward(
         self, audio, aligntext: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        logits = self.encode(audio)
+        hasf0_logits, f0_hat, logspc_hat, codeap_hat = self.decode(aligntext)
+        return logits, hasf0_logits, f0_hat, logspc_hat, codeap_hat
+
+    def encode(
+        self, audio: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-
         audio = torch.transpose(audio, 1, 2)
-        enc_out = self.encoder(audio)
+        enc_out = self.voice_encoder(audio)
         enc_out = torch.transpose(enc_out, 1, 2)
-        logits = self.encoder_proj(enc_out)
+        logits = self.encode_projection(enc_out)
+        # logits: [batch_size, audio_len, vocab_size]
+        return logits
 
-        x = self.embedding(logits)
+    def decode(
+        self, aligntext: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        x = self.embedding(aligntext)
         x = torch.transpose(x, 1, 2)
-        x = self.decoder(x)
+        x = self.voice_decoder(x)
         x = torch.transpose(x, 1, 2)
-        # world_out: [batch_size, target_len, audio_size]
+        # world_out: [batch_size, target_len, target_audio_size]
 
         hasf0_logits, f0_hat, logspc_hat, codeap_hat = torch.split(x, [
             self.hasf0_size,
@@ -259,11 +286,14 @@ class AudioToAudioModel(pl.LightningModule):
         f0_hat = f0_hat[:, :, 0]
         return hasf0_logits, f0_hat, logspc_hat, codeap_hat
 
+    def encode_projection(self, enc_out: torch.Tensor) -> torch.Tensor:
+        return enc_out @ self.embedding.weight.T
+
     def predict(
         self, aligntext: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
 
-        hasf0, f0, logspc, codeap = self.forward(aligntext)
+        _, hasf0, f0, logspc, codeap = self.forward(aligntext)
         f0, logspc, codeap = self.norm.unnormalize(f0, logspc, codeap)
         f0 = torch.where(
             hasf0 < 0, torch.zeros(size=(1,), dtype=f0.dtype, device=f0.device), f0
@@ -279,31 +309,32 @@ class AudioToAudioModel(pl.LightningModule):
         hasf0 = (f0 >= 30.0).to(torch.float32)
         f0, logspc, codeap = self.norm.normalize(f0, logspc, codeap)
 
-        hasf0_logits, f0_hat, logspc_hat, codeap_hat = self.forward(audio, aligntext)
-
-        hasf0_loss, f0_loss, logspc_loss, codeap_loss = self.criteria(
+        logits, hasf0_logits, f0_hat, logspc_hat, codeap_hat = self.forward(audio, aligntext)
+        encoder_loss = self.encoder_loss(logits, aligntext, aligntext_len)
+        hasf0_loss, f0_loss, logspc_loss, codeap_loss = self.decoder_loss(
             f0_len, hasf0_logits, f0_hat, logspc_hat, codeap_hat, hasf0, f0, logspc, codeap)
 
-        return hasf0_loss, f0_loss, logspc_loss, codeap_loss
+        return encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss
 
     def training_step(self, batch, batch_idx) -> torch.Tensor:
-        hasf0_loss, f0_loss, logspc_loss, codeap_loss = self._calc_batch_loss(batch)
-        loss = hasf0_loss + f0_loss + logspc_loss + codeap_loss
-        self._log_loss('train', loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss)
+        encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss = self._calc_batch_loss(batch)
+        loss = encoder_loss + hasf0_loss + f0_loss + logspc_loss + codeap_loss
+        self._log_loss('train', loss, encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        hasf0_loss, f0_loss, logspc_loss, codeap_loss = self._calc_batch_loss(batch)
-        loss = hasf0_loss + f0_loss + logspc_loss + codeap_loss
-        self._log_loss('val', loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss)
+        encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss = self._calc_batch_loss(batch)
+        loss = encoder_loss + hasf0_loss + f0_loss + logspc_loss + codeap_loss
+        self._log_loss('val', loss, encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss)
 
     def test_step(self, batch, batch_idx):
-        hasf0_loss, f0_loss, logspc_loss, codeap_loss = self._calc_batch_loss(batch)
-        loss = hasf0_loss + f0_loss + logspc_loss + codeap_loss
-        self._log_loss('test', loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss)
+        encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss = self._calc_batch_loss(batch)
+        loss = encoder_loss + hasf0_loss + f0_loss + logspc_loss + codeap_loss
+        self._log_loss('test', loss, encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss)
 
-    def _log_loss(self, task, loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss) -> None:
+    def _log_loss(self, task, loss, encoder_loss, hasf0_loss, f0_loss, logspc_loss, codeap_loss) -> None:
         self.log(f'{task}_loss', loss)
+        self.log(f'{task}_encoder_loss', encoder_loss)
         self.log(f'{task}_hasf0_loss', hasf0_loss)
         self.log(f'{task}_f0_loss', f0_loss)
         self.log(f'{task}_logspc_loss', logspc_loss)
