a4f1beeba763fca75cb4fa01a9a9a20596434432
Katsuya Iida <katsuya.iida@gmail.com>
2022-12-30 07:27:19 +0000

    JapanesePhonemizer removes corrput tokens.

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -219,35 +219,40 @@ class DatasetTest(unittest.TestCase):
 
 @pytest.mark.skip("dataset are needed")
 def test_data_module():
-    ARGS = "--dataset kokoro_small --language ja --use_phone --vocoder mel"
+    from voice100.audio import BatchSpectrogramAugumentation
+    ARGS = "--dataset cv_ja --language ja --use_phone --vocoder mel"
     parser = argparse.ArgumentParser()
     AudioTextDataModule.add_argparse_args(parser)
     args = parser.parse_args(ARGS.split())
     data: AudioTextDataModule = AudioTextDataModule.from_argparse_args(args)
     data.setup()
+    spec_augment = BatchSpectrogramAugumentation()
 
     print(f"audio_size={data.audio_size}")
     print(f"vocab_size={data.vocab_size}")
     use_phone = '--use_phone' in ARGS
-    expected_vocab_size = 77 if use_phone else 29
+    expected_vocab_size = 44 if use_phone else 29
     expected_audio_size = 64
     assert data.vocab_size == expected_vocab_size
     assert data.audio_size == expected_audio_size
 
+    counter = [0] * data.vocab_size
     for batch in tqdm(data.train_dataloader()):
         # print(batch)
         with torch.no_grad():
             (audio, audio_len), (text, text_len) = batch
+            audio, audio_len = spec_augment(audio, audio_len)
             packed_audio = pack_padded_sequence(audio, audio_len, batch_first=True, enforce_sorted=False)
             _ = pad_packed_sequence(packed_audio, batch_first=False)
             assert not (torch.any(audio.isnan()))
+            assert not (torch.any(audio.isinf()))
             assert (torch.min(audio_len) > 0)
             assert (torch.max(audio_len) == audio.shape[1]), f'{audio_len} {audio.shape}'
-
             for i in range(text.shape[0]):
-                t = text[i, :text_len[i]]
-                print(data.text_transform.tokenizer.decode(t))
-        break
+                assert text_len[i] > 0
+                for j in range(text_len[i]):
+                    counter[text[i, j]] += 1
+            assert counter[0] == 0, text
 
 
 @pytest.mark.skip("dataset are needed")
