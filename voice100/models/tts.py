88813ca5757f734323d30dfd66aac94c5a1ad496
Katsuya Iida <katsuya.iida@gmail.com>
2023-01-07 21:14:36 +0900

    update

--- voice100/models/tts.py
+++ voice100/models/tts.py
@@ -488,6 +488,6 @@ class AlignTextToAudioMultiTaskModel(pl.LightningModule):
             learning_rate=args.learning_rate,
             **kwargs)
         if not args.resume_from_checkpoint:
-            args.audio_stat = f'./data/{args.dataset}-stat.pt'
+            args.audio_stat = './data/audio-stat.pt'
             model.norm.load_state_dict(torch.load(args.audio_stat))
         return model
