f7ad4b561d40ec76fc7503ea2477287ebd51d1c4
Katsuya Iida <katsuya.iida@gmail.com>
2023-01-23 20:11:31 +0900

    update

--- voice100/main.py
+++ voice100/main.py
@@ -1,7 +1,9 @@
-# main.py
+# Copyright (C) 2023 Katsuya Iida. All rights reserved.
+
 from pytorch_lightning.cli import LightningCLI
 import voice100.models  # noqa: F401
 import voice100.data_modules  # noqa: F401
+
 
 def cli_main():
     cli = LightningCLI()
